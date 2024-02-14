a188d24a6043d69e016ac482c1ec8f4920512e22
ravi688 <rp0412204@gmail.com>
2024-02-14 07:31:04 +0530

    Added support for YUV images

--- source/main.c
+++ source/main.c
@@ -359,13 +359,13 @@ int main()
 														VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU, 
 														VK_FORMAT_B8G8R8A8_SRGB, 
 														VK_COLOR_SPACE_SRGB_NONLINEAR_KHR, 
-														VK_PRESENT_MODE_FIFO_KHR, 3);
+														VK_PRESENT_MODE_FIFO_KHR, 3, false);
 	uint32_t graphicsQueueFamilyIndex = pvkFindQueueFamilyIndex(physicalGPU, VK_QUEUE_GRAPHICS_BIT);
 	uint32_t presentQueueFamilyIndex = pvkFindQueueFamilyIndexWithPresentSupport(physicalGPU, surface);
 	uint32_t queueFamilyIndices[2] = { graphicsQueueFamilyIndex, presentQueueFamilyIndex };
 	VkDevice logicalGPU = pvkCreateLogicalDeviceWithExtensions(instance, 
 																physicalGPU,
-																2, queueFamilyIndices,
+																2, queueFamilyIndices, false,
 																1, VK_KHR_SWAPCHAIN_EXTENSION_NAME);
 	VkQueue graphicsQueue, presentQueue;
 	vkGetDeviceQueue(logicalGPU, graphicsQueueFamilyIndex, 0, &graphicsQueue);
